65231f0e172ec6cf98912dc98775cbd781c4d8df
Oussama Nouma <oussama.nouma@learner.42.tech>
2026-06-07 14:16:27 +0200

    fix: Data_Archivist all exercises reviewed and tested

--- Data_Archivist/ex0/ft_ancient_text.py
+++ Data_Archivist/ex0/ft_ancient_text.py
@@ -4,17 +4,22 @@ import typing
 
 if __name__ == "__main__":
     try:
-        if len(sys.argv) == 1 or len(sys.argv) > 2:
+        if len(sys.argv) != 2:
             raise ValueError("Usage: ft_ancient_text.py <file>")
         print("=== Cyber Archives Recovery ===")
-        print(f"Accessing file ’{sys.argv[1]}’")
+        print(f"Accessing file '{sys.argv[1]}'")
         f: typing.IO[str] = open(sys.argv[1], "r")
         to_print: str = f.read()
         print(f"---\n{to_print}\n---")
         f.close()
-        print(f"File ’{sys.argv[1]}’ closed.")
-    except (FileNotFoundError, PermissionError, ValueError) as err:
-        if len(sys.argv) == 1:
+        print(f"File '{sys.argv[1]}' closed.")
+    except (
+        FileNotFoundError,
+        PermissionError,
+        ValueError,
+        IsADirectoryError
+            ) as err:
+        if len(sys.argv) != 2:
             print(err)
         else:
-            print(f"Error opening file ’{sys.argv[1]}’: {err}")
+            print(f"Error opening file '{sys.argv[1]}': {err}")
--- Data_Archivist/ex1/ft_archive_creation.py
+++ Data_Archivist/ex1/ft_archive_creation.py
@@ -5,15 +5,24 @@ import typing
 if __name__ == "__main__":
     f_name: str = ""
     try:
-        if len(sys.argv) == 1 or len(sys.argv) > 2:
+        if len(sys.argv) != 2:
             raise ValueError("Usage: ft_ancient_text.py <file>")
         print("=== Cyber Archives Recovery & Preservation ===")
-        print(f"Accessing file ’{sys.argv[1]}’")
-        f: typing.IO[str] = open(sys.argv[1], "r")
-        to_print: str = f.read()
-        print(f"---\n{to_print}\n---")
-        f.close()
-        print(f"File ’{sys.argv[1]}’ closed.")
+        print(f"Accessing file '{sys.argv[1]}'")
+        try:
+            f: typing.IO[str] = open(sys.argv[1], "r")
+            to_print: str = f.read()
+            print(f"---\n{to_print}\n---")
+            f.close()
+            print(f"File '{sys.argv[1]}' closed.")
+        except FileNotFoundError as err:
+            raise FileNotFoundError(f"Error opening file "
+                                    f"'{sys.argv[1]}': {err}")
+        except PermissionError as err:
+            raise PermissionError(f"Error opening file '{sys.argv[1]}': {err}")
+        except IsADirectoryError as err:
+            raise IsADirectoryError(f"Error opening file "
+                                    f"'{sys.argv[1]}': {err}")
         transformed: list[str] = [line + "#" for line in to_print.splitlines()]
         new_data: str = "\n".join(transformed)
         if to_print.endswith("\n"):
@@ -23,15 +32,22 @@ if __name__ == "__main__":
         if not f_name:
             raise ValueError("Not saving data.")
         print(f"Saving data to {f_name}")
-        new_f: typing.IO[str] = open(f_name, "w")
-        new_f.write(new_data)
-        print(f"Data saved in file {f_name}.")
-        new_f.close()
-        print(f"File ’{f_name}’ closed.")
-    except (FileNotFoundError, PermissionError, ValueError) as err:
-        if len(sys.argv) == 1:
-            print(err)
-        elif not f_name:
-            print(err)
-        else:
-            print(f"Error opening file ’{sys.argv[1]}’: {err}")
+        try:
+            new_f: typing.IO[str] = open(f_name, "w")
+            new_f.write(new_data)
+            print(f"Data saved in file {f_name}.")
+            new_f.close()
+            print(f"File '{f_name}' closed.")
+        except FileNotFoundError as err:
+            raise FileNotFoundError(f"Error opening file '{f_name}': {err}")
+        except PermissionError as err:
+            raise PermissionError(f"Error opening file '{f_name}': {err}")
+        except IsADirectoryError as err:
+            raise IsADirectoryError(f"Error opening file '{f_name}': {err}")
+    except (
+            FileNotFoundError,
+            PermissionError,
+            ValueError,
+            IsADirectoryError
+            ) as err:
+        print(err)
