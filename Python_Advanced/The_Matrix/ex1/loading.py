d5b7cb5ea29714b99372fbba46bb000f4ca2e82c
Oussama Nouma <oussama.nouma@learner.42.tech>
2026-06-16 19:54:21 +0200

    feat: Generated, formatted and visualized Matrix data as scatter plot

--- Python_Advanced/The_Matrix/ex1/loading.py
+++ Python_Advanced/The_Matrix/ex1/loading.py
@@ -3,22 +3,45 @@ import importlib.metadata as md
 
 
 def check_for_dependencies() -> bool:
-    dependencies: list[str] = ["pandas", "numpy", "requests", "matplotlib"]
+    dependencies: list[str] = ["pandas", "numpy",
+                               "matplotlib"]
     success: bool = True
     for x in dependencies:
         try:
-            lib = lb.import_module(x)
-            print(f"[OK] {x} {md.version(x)} - {md.metadata(x)["Summary"]}")
+            lb.import_module(x)
+            summary = md.metadata(x)["Summary"]
+            print(f"[OK] {x} {md.version(x)} - {summary}")
         except ImportError as err:
-            print(f"[KO] {err} - Install it with pip3 install {x}")
+            print(f"[KO] {err} - Install it with pip3 install {x}"
+                  f"or Install it with poetry add {x}")
             success = False
-        else:
-            globals()[x] = lib
     return success
 
 
-check_for_dependencies()
-
-
-def data_generator() -> None:
-    np = numpy
+if __name__ == "__main__":
+    print("""LOADING STATUS: Loading programs...
+Checking dependencies:""")
+    if check_for_dependencies():
+        import numpy as np
+        import pandas as pd
+        import matplotlib.pyplot as plt  # type: ignore
+        player_hp: np.ndarray = np.random.randint(50, 250, size=1000)
+        boss_hp: np.ndarray = np.random.randint(350, 650, size=1000)
+        success_rate: np.ndarray = \
+            np.round(((player_hp + 100) / boss_hp) * 100, 1)
+        result = np.column_stack((player_hp, boss_hp, success_rate))
+        frame = pd.DataFrame(result,
+                             columns=["Player_hp",
+                                      "Boss_hp", "Success_rate"])
+        print("""\nAnalyzing Matrix data...
+Processing 1000 data points...
+Generating visualization...\n""")
+        plt.scatter(frame["Player_hp"], frame["Success_rate"],
+                    s=10, c=frame["Boss_hp"], cmap="plasma")
+        plt.colorbar(label="Boss HP")
+        plt.title("Player vs Boss")
+        plt.xlabel("Player Health")
+        plt.ylabel("Win rate")
+        plt.savefig("matrix_analysis.png")
+        print("""Analysis complete!
+Results saved to: matrix_analysis.png""")
